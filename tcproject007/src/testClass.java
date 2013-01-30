fb9239eb6136f74c37b536d6c0d6ece8efcc9404
michailr <michail.rallis@haysystems.com>
2013-01-30 14:32:47 +0000

    asda
    asda

---
+++ tcproject007/src/testClass.java
@@ -0,0 +1,25 @@
+
+public class testClass {
+	
+	private String name;
+	private int age;
+	
+	public testClass(String nameIn, int ageIn){
+		name = nameIn;
+		age = ageIn;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getAge(){
+		return age;
+	}
+
+	
+	public static void main(String args[]){
+		testClass obj = new testClass("Michail", 100);
+		System.out.println(obj.getName() + " " + obj.getAge());
+	}
+}
